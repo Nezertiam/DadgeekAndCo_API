66d189a55e151262ba37bf99ff6aee5071e54c58
Nezertiam <mawyn.nhek@gmail.com>
2021-10-18 19:42:32 +0200

    [FIX] Security breach with token's payload role based

--- controllers/article.controller.js
+++ controllers/article.controller.js
@@ -1,13 +1,13 @@
 import { validationResult } from "express-validator";
-import Article from "../models/Article.js"
+import Article from "../models/Article.js";
+import User from "../models/User.js";
 import slugify from "slugify";
+import isGranted from "../services/isGranted.js";
+import mongoose from "mongoose";
 
 // @route /api/article
 /**
  * Create a new article
- * 
- * @param {*} req 
- * @param {*} res 
  */
 export const createArticle = async (req, res) => {
     // First, validate body content or return an error
@@ -16,13 +16,19 @@ export const createArticle = async (req, res) => {
         return res.status(400).json({ errors: errors.array() });
     }
 
-    // Get the body content and hydrate a new article object, then save it
+    // Get the user with the id in the token
+    const user = await User.findOne({ _id: req.user.id });
+    if (!user) return res.status(404).json({ message: "Can't authenticate this user" });
+
+    // Only Authors and Admin can create an article
+    const permission = isGranted("ROLE_AUTHOR", user);
+    if (!permission) return res.status(400).json({ message: "You don't have the permission to do that" });
+
+    // Get the body content, slug the title and hydrate a new article object, then save it
     const { title, description, blocks } = req.body;
     const slug = slugify(title);
-
     const article = await Article.findOne({ slug: slug });
     if (article) return res.status(400).json({ message: "Title already taken. Maybe you already published this article?" });
-
     try {
         const article = new Article({
             user: req.user.id,
@@ -43,17 +49,16 @@ export const createArticle = async (req, res) => {
 // @route /api/article/:slug
 /**
  * Get and return the article by the slug
- * 
- * @param {*} req 
- * @param {*} res 
  */
 export const readArticle = async (req, res) => {
+    // Get the slug for db search
     const slug = req.params.slug;
 
+    // Get the article with the slug
     const article = await Article.findOne({ slug: slug });
-
     if (!article) return res.status(404).json({ message: "Article not found" });
 
+    // Return the article
     return res.json({ article })
 }
 
@@ -61,22 +66,27 @@ export const readArticle = async (req, res) => {
 // @route /api/article/:slug
 /**
  * Edit an article found by its slug
- * 
- * @param {*} req 
- * @param {*} res 
- * @returns 
  */
 export const editArticle = async (req, res) => {
     // First, validate body content or return an error
     const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() });
-    }
+    if (!errors.isEmpty()) return res.status(400).json({ errors: errors.array() });
 
+    // Get the user with the id in the token
+    const user = await User.findOne({ _id: req.user.id });
+    if (!user) return res.status(404).json({ message: "Can't authenticate this user" });
+
+    // find article by slug
     const slug = req.params.slug;
     const article = await Article.findOne({ slug: slug });
-
     if (!article) return res.status(404).json({ message: "Article not found" });
+
+    // Only the author can modify its articles
+    if (!user.equals(article.user)) return res.status(400).json({ message: "You can't modify an article that isn't yours" });
+
+    // The user must still be an anthor to be able to modify its articles
+    if (!user.roles.includes("ROLE_AUTHOR")) return res.status(400).json({ message: "You can't modify content since you're not an author anymore" })
+
 
     const articleFields = {};
     const { description, blocks } = req.body;
@@ -93,7 +103,6 @@ export const editArticle = async (req, res) => {
         return res.status(200).json(editedArticle);
     } catch (err) {
         console.error(err.message);
-        res.status(500).json({ message: "Server error." })
+        return res.status(500).json({ message: "Server error." })
     }
-
 }
